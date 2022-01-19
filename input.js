592f20536d79c27858c287aa87495cb43c67bd1a
mrjohnming <john.ngai@hotmail.com>
2022-01-19 20:18:37 +0000

    Moved & modularized the setupInput and handleUserInput functions into it's own file.

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  // event listener
+  stdin.on('data', handleUserInput);
+  
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  // \u003 = ctrl + c
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput};
