4cf66426ee5906dde1567eca81d0ac2172fe487a
mrjohnming <john.ngai@hotmail.com>
2022-01-19 20:20:14 +0000

    Cleaned up the code by removing some functions and adding them back in as modules.

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  // event listener
-  stdin.on('data', handleUserInput);
-  
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  // \u003 = ctrl + c
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
